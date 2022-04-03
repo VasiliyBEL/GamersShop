a1aefac21f856406fb533d7d01fe73321865ace7
VasiliyBEL <vaspn001@gmail.com>
2022-04-03 05:12:41 +0300

    Add GameController.cs

---
+++ GamersShop.WebUI/Controllers/GameController.cs
@@ -0,0 +1,23 @@
+﻿using GamersShop.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamersShop.WebUI.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly IGameRepository repository;
+        public GameController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Games);
+        }
+    }
+}
