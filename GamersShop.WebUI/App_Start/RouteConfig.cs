8c240cc306258044ae50fb98725c36786dba8f15
VasiliyBEL <vaspn001@gmail.com>
2022-04-04 19:30:51 +0300

    Configure routes.

--- GamersShop.WebUI/App_Start/RouteConfig.cs
+++ GamersShop.WebUI/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace GamersShop.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new {controller = "Game", action = "List"}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Game", action = "List", id = UrlParameter.Optional }
