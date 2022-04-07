0e2824ba82123a58be1dfb8763881b099cfe1d65
VasiliyBEL <vaspn001@gmail.com>
2022-04-07 16:05:49 +0300

    Create Navigation Controller for selection categories.

---
+++ GamersShop.WebUI/Controllers/NavController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamersShop.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+        public string Menu()
+        {
+            return "Тестируем контроллер Nav";
+        }
+    }
+}
