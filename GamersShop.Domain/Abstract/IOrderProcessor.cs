ded29e00f2e376f702c2fb1b6e938a95bcac233f
VasiliyBEL <vaspn001@gmail.com>
2022-04-09 22:32:41 +0300

    Implement interface about procesing buying.

---
+++ GamersShop.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,14 @@
+﻿using GamersShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamersShop.Domain.Abstract
+{
+    public interface IOrderProcessor
+    {
+        void ProcessOrder(Cart cart, ShippingDetail shippinDetail);
+    }
+}
