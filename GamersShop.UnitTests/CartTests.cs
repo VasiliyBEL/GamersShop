a8db7c04fdf20c7d58f12ab63ba2d436ef689909
VasiliyBEL <vaspn001@gmail.com>
2022-04-09 12:34:35 +0300

    Create Test for sum up Cost of all elements in cart.

--- GamersShop.UnitTests/CartTests.cs
+++ GamersShop.UnitTests/CartTests.cs
@@ -76,5 +76,25 @@ namespace GamersShop.UnitTests
             Assert.AreEqual(cart.Lines.Where(c => c.Game == game2).Count(), 0);
             Assert.AreEqual(cart.Lines.Count(), 2);
         }
+
+        [TestMethod]
+        public void Calculate_Cart_Total()
+        {
+            // Act
+            Game game1 = new Game { GameId = 1, Name = "Игра1", Price = 100 };
+            Game game2 = new Game { GameId = 2, Name = "Игра2", Price = 55 };
+
+            // Act
+            Cart cart = new Cart();
+
+            // Arrange
+            cart.AddItem(game1, 1);
+            cart.AddItem(game2, 1);
+            cart.AddItem(game1, 5);
+            decimal result = cart.ComputeTotalValue();
+
+            // Assert
+            Assert.AreEqual(result, 655);
+        }
     }
 }
