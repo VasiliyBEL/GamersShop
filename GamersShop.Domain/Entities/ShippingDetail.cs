db416fe5da0be3a774be386c7fc441cf807e62d5
VasiliyBEL <vaspn001@gmail.com>
2022-04-09 20:57:34 +0300

    Create Model about shipping of the game.

---
+++ GamersShop.Domain/Entities/ShippingDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamersShop.Domain.Entities
+{
+    public class ShippingDetail
+    {
+        [Required(ErrorMessage = "Укажите как вас зовут")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Вставьте первый адрес доставки")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Укажите город")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Укажите страну")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
