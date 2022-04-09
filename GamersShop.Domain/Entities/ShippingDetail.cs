b41fd797660ae1f0cec589eb0b8bdde109c6f32b
VasiliyBEL <vaspn001@gmail.com>
2022-04-09 22:24:56 +0300

    Correct view of ShippingDetail.

--- GamersShop.Domain/Entities/ShippingDetail.cs
+++ GamersShop.Domain/Entities/ShippingDetail.cs
@@ -13,14 +13,19 @@ namespace GamersShop.Domain.Entities
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Вставьте первый адрес доставки")]
+        [Display(Name = "Первый адрес")]
         public string Line1 { get; set; }
+        [Display(Name = "Второй адрес")]
         public string Line2 { get; set; }
+        [Display(Name = "Третий адрес")]
         public string Line3 { get; set; }
 
         [Required(ErrorMessage = "Укажите город")]
+        [Display(Name = "Город")]
         public string City { get; set; }
 
         [Required(ErrorMessage = "Укажите страну")]
+        [Display(Name = "Страна")]
         public string Country { get; set; }
 
         public bool GiftWrap { get; set; }
