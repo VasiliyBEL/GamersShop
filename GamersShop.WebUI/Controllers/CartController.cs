f21ab3f5595236834c59e3248451829e9e93d771
VasiliyBEL <vaspn001@gmail.com>
2022-04-09 13:04:31 +0300

    Create CartController for activities of user.

---
+++ GamersShop.WebUI/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using GamersShop.Domain.Abstract;
+using GamersShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamersShop.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IGameRepository repository;
+
+        // GET: Cart
+        public CartController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public RedirectToRouteResult AddToCart(int gameId, string returnUrl)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                GetCart().AddItem(game, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int gameId, string returnUrl)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                GetCart().RemoveLine(game);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
