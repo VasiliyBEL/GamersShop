eaaddec82d36e24f34801518908a746137fa9dbe
VasiliyBEL <vaspn001@gmail.com>
2022-04-09 11:50:20 +0300

    Create Test for adding quantity of selected items.

--- GamersShop.UnitTests/CartTests.cs
+++ GamersShop.UnitTests/CartTests.cs
@@ -1,6 +1,5 @@
 ﻿using GamersShop.Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using static GamersShop.Domain.Entities.Cart;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +28,28 @@ namespace GamersShop.UnitTests
             Assert.AreEqual(results.Count(), 2);
             Assert.AreEqual(results[0].Game, game1);
             Assert.AreEqual(results[1].Game, game2);
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            // Act
+            Game game1 = new Game { GameId = 1, Name = "Игра1" };
+            Game game2 = new Game { GameId = 2, Name = "Игра2" };
+
+            // Act
+            Cart cart = new Cart();
+
+            // Arrange
+            cart.AddItem(game1, 1);
+            cart.AddItem(game2, 1);
+            cart.AddItem(game1, 5);
+            List<CartLine> results = cart.Lines.OrderBy(c => c.Game.GameId).ToList();
+
+            // Assert
+            Assert.AreEqual(results.Count(), 2);
+            Assert.AreEqual(results[0].Quantity, 6);    // 6 экземпляров добавлено в корзину
+            Assert.AreEqual(results[1].Quantity, 1);
         }
     }
 }
