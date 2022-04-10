2b127bcf3f2e1a36c4ee0b3fa730fe265b180ffc
VasiliyBEL <vaspn001@gmail.com>
2022-04-10 17:41:42 +0300

    Create AdminController.

---
+++ GamersShop.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using GamersShop.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamersShop.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        readonly IGameRepository repository;
+
+        // GET: Admin
+        public AdminController (IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Games);
+        }
+    }
+}
