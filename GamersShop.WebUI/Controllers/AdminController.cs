40145d2b05baa4fa0d1e35e718ea66e7987cbfe0
VasiliyBEL <vaspn001@gmail.com>
2022-04-10 22:15:09 +0300

    View edit by returning ActionResult View.

--- GamersShop.WebUI/Controllers/AdminController.cs
+++ GamersShop.WebUI/Controllers/AdminController.cs
@@ -29,5 +29,22 @@ namespace GamersShop.WebUI.Controllers
                 .FirstOrDefault(g => g.GameId == gameId);
             return View(game);
         }
+
+        // Перегруженная версия Edit() для сохранения изменений
+        [HttpPost]
+        public ActionResult Edit(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveGame(game);
+                TempData["message"] = string.Format("Изменения в игре \"{0}\" были сохранены", game.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Что-то не так со значениями данных
+                return View(game);
+            }
+        }
     }
 }
