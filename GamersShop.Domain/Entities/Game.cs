0a8d9ede2c292e33313c92a87a2f0a8f379ed3b6
VasiliyBEL <vaspn001@gmail.com>
2022-04-10 23:12:23 +0300

    View Error Messages.

--- GamersShop.Domain/Entities/Game.cs
+++ GamersShop.Domain/Entities/Game.cs
@@ -14,16 +14,21 @@ namespace GamersShop.Domain.Entities
         public int GameId { get; set; }
 
         [Display(Name = "Название")]
+        [Required(ErrorMessage = "Пожалуйста, введите название игры")]
         public string Name { get; set; }
 
         [DataType(DataType.MultilineText)]
         [Display(Name = "Описание")]
+        [Required(ErrorMessage = "Пожалуйста, введите описание для игры")]
         public string Description { get; set; }
 
         [Display(Name = "Категория")]
+        [Required(ErrorMessage = "Пожалуйста, укажите категорию для игры")]
         public string Category { get; set; }
 
         [Display(Name = "Цена (руб)")]
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите положительное значение для цены")]
         public decimal Price { get; set; }
     }
 }
