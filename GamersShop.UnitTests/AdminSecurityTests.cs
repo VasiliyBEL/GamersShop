baf9768066a42dfe439158683505e58041a71eac
VasiliyBEL <vaspn001@gmail.com>
2022-04-11 06:09:43 +0300

    Create UnitTests to login with valid credentials and not login with invalid credentials.

---
+++ GamersShop.UnitTests/AdminSecurityTests.cs
@@ -0,0 +1,64 @@
+﻿using GamersShop.WebUI.Controllers;
+using GamersShop.WebUI.Infrastructure.Abstract;
+using GamersShop.WebUI.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Web.Mvc;
+
+namespace GamersShop.UnitTests
+{
+    [TestClass]
+    public class AdminSecurityTests
+    {
+        [TestMethod]
+        public void Can_Login_With_Valid_Credentials()
+        {
+            // Arrange
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin", "12345")).Returns(true);
+
+            // Arrange
+            LoginViewModel model = new LoginViewModel
+            {
+                UserName = "admin",
+                Password = "12345"
+            };
+
+            // Arrange
+            AccountController target = new AccountController(mock.Object);
+
+            // Act
+            ActionResult result = target.Login(model, "/MyURL");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectResult));
+            Assert.AreEqual("/MyURL", ((RedirectResult)result).Url);
+        }
+
+        [TestMethod]
+        public void Cannot_Login_With_Invalid_Credentials()
+        {
+            // Arrange
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("badUser", "badPass")).Returns(false);
+
+            // Arrange
+            LoginViewModel model = new LoginViewModel
+            {
+                UserName = "badUser",
+                Password = "badPass"
+            };
+
+            // Arrange
+            AccountController target = new AccountController(mock.Object);
+
+            // Act
+            ActionResult result = target.Login(model, "/MyURL");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.IsFalse(((ViewResult)result).ViewData.ModelState.IsValid);
+        }
+    }
+}
