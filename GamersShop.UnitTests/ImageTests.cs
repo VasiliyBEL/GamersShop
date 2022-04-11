20ac2b8de0d29a7fe49e139aaa24b8352914560b
VasiliyBEL <vaspn001@gmail.com>
2022-04-11 09:08:07 +0300

    Create UnitTests for Retrieve Image Data and Cannot retrieve image data for invalid ID.

---
+++ GamersShop.UnitTests/ImageTests.cs
@@ -0,0 +1,68 @@
+﻿using GamersShop.Domain.Abstract;
+using GamersShop.Domain.Entities;
+using GamersShop.WebUI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GamersShop.UnitTests
+{
+    [TestClass]
+    public class ImageTests
+    {
+        [TestMethod]
+        public void Can_Retrieve_Image_Data()
+        {
+            // Arrange
+            Game game = new Game
+            {
+                GameId = 2,
+                Name = "Игра2",
+                ImageData = new byte[] { },
+                ImageMimeType = "image/png"
+            };
+
+            // Arrange
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game> {
+                new Game {GameId = 1, Name = "Игра1"},
+                game,
+                new Game {GameId = 3, Name = "Игра3"}
+            }.AsQueryable());
+
+            // Arrange
+            GameController controller = new GameController(mock.Object);
+
+            // Act
+            ActionResult result = controller.GetImage(2);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(FileResult));
+            Assert.AreEqual(game.ImageMimeType, ((FileResult)result).ContentType);
+        }
+
+        [TestMethod]
+        public void Cannot_Retrieve_Image_Data_For_Invalid_ID()
+        {
+            // Arrange
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game> {
+                new Game {GameId = 1, Name = "Игра1"},
+                new Game {GameId = 2, Name = "Игра2"}
+            }.AsQueryable());
+
+            // Arrange
+            GameController controller = new GameController(mock.Object);
+
+            // Act
+            ActionResult result = controller.GetImage(10);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
