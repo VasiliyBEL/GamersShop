a321964996a71b96981714b800a0c98d00bd5752
VasiliyBEL <vaspn001@gmail.com>
2022-04-11 05:16:45 +0300

    Create Account Controller.

---
+++ GamersShop.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using GamersShop.WebUI.Infrastructure.Abstract;
+using GamersShop.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamersShop.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        readonly IAuthProvider authProvider;
+        // GET: Account
+        public AccountController(IAuthProvider auth)
+        {
+            authProvider = auth;
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (authProvider.Authenticate(model.UserName, model.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин или пароль");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
